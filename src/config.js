ba3d9520c337b3e34f8f21dfe36f1257e1a86615
Franke <henriquefranke31@gmail.com>
2026-01-29 18:19:59 -0300

    sg

--- src/config.js
+++ src/config.js
@@ -1,13 +1,13 @@
-export const CONFIG = {
+const CONFIG = {
   BOT_TOKEN: process.env.BOT_TOKEN,
   GROUP_ID: Number(process.env.GROUP_ID),
 
   MP_ACCESS_TOKEN: process.env.MP_ACCESS_TOKEN,
   MP_PUBLIC_KEY: process.env.MP_PUBLIC_KEY,
 
-  VALOR_VIP: Number(process.env.VALOR_VIP || 29.9),
-  DIAS_VIP: Number(process.env.DIAS_VIP || 30),
-
-  PORT: Number(process.env.PORT || 8080),
-  DATABASE_URL: process.env.DATABASE_URL
+  VALOR_VIP: 29.9,
+  DIAS_VIP: 30,
+  PORT: 8080
 };
+
+export default CONFIG;
