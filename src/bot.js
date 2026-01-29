7c01f1355c1d36717559703016487ce3ec9847f2
Franke <henriquefranke31@gmail.com>
2026-01-29 19:43:27 -0300

    sg

--- src/bot.js
+++ src/bot.js
@@ -1,5 +1,5 @@
 import TelegramBot from "node-telegram-bot-api"
-import config from "./config.js"
+import * as config from "./config.js"
 import { criarAssinatura } from "./mp.js"
 
 console.log("🤖 BOT.JS CARREGADO")
