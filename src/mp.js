3a086816f448ca1d54bcc1f41363ac6b5efd61d2
Franke <henriquefranke31@gmail.com>
2026-01-30 23:53:14 -0300

    sgk

--- src/mp.js
+++ src/mp.js
@@ -6,21 +6,25 @@ mercadopago.configure({
 })
 
 export async function criarAssinatura(telegramId) {
+  try {
+    const response = await mercadopago.preapproval.create({
+      reason: "VIP Telegram",
+      external_reference: String(telegramId),
+      auto_recurring: {
+        frequency: 1,
+        frequency_type: "months",
+        transaction_amount: CONFIG.VIP_PRICE,
+        currency_id: "BRL"
+      },
+      payer_email: CONFIG.FIXED_PAYER_EMAIL,
+      back_url: "https://google.com"
+    })
 
-  const response = await mercadopago.preapproval.create({
-    reason: "VIP Telegram",
-    external_reference: String(telegramId),
+    console.log("MP RESPONSE:", response.body)
 
-    auto_recurring: {
-      frequency: 1,
-      frequency_type: "months",
-      transaction_amount: CONFIG.VIP_PRICE,
-      currency_id: "BRL"
-    },
-
-    payer_email: CONFIG.FIXED_PAYER_EMAIL,
-    back_url: CONFIG.BACK_URL
-  })
-
-  return response.body.init_point
+    return response.body.init_point // 👈 ESSA LINHA
+  } catch (err) {
+    console.error("❌ Erro MP assinatura:", err)
+    throw new Error("Erro ao gerar assinatura")
+  }
 }
