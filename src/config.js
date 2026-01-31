01ddb8d45d4c76239b20f0c870229a61486d09fc
Franke <henriquefranke31@gmail.com>
2026-01-30 22:47:24 -0300

    sg

--- src/config.js
+++ src/config.js
@@ -4,6 +4,8 @@ export const CONFIG = {
   TELEGRAM_TOKEN: process.env.TELEGRAM_TOKEN,
   VIP_GROUP_ID: process.env.VIP_GROUP_ID,
 
+  BOT_TOKEN: process.env.BOT_TOKEN,
+
   MP_ACCESS_TOKEN: process.env.MP_ACCESS_TOKEN,
 
   VIP_PRICE: Number(process.env.VIP_PRICE || 29.9),
