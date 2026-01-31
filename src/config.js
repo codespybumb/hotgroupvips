158e5d1e1eceabc15c1f449d965131ef9b1b577b
Franke <henriquefranke31@gmail.com>
2026-01-30 22:44:45 -0300

    sg

--- src/config.js
+++ src/config.js
@@ -1,25 +1,13 @@
+// src/config.js
+
 export const CONFIG = {
-  BOT_TOKEN: process.env.BOT_TOKEN,
-  GROUP_ID: Number(process.env.GROUP_ID),
-    VIP_PRICE: Number(process.env.VIP_PRICE || 29.9),
+  TELEGRAM_TOKEN: process.env.TELEGRAM_TOKEN,
+  VIP_GROUP_ID: process.env.VIP_GROUP_ID,
+
   MP_ACCESS_TOKEN: process.env.MP_ACCESS_TOKEN,
-  FIXED_PAYER_EMAIL: process.env.FIXED_PAYER_EMAIL
-  BASE_URL: process.env.BASE_URL,
-  PORT: Number(process.env.PORT || 8080)
-}
 
+  VIP_PRICE: Number(process.env.VIP_PRICE || 29.9),
 
-
-// validações obrigatórias
-const required = [
-  "BOT_TOKEN",
-  "GROUP_ID",
-  "MP_ACCESS_TOKEN",
-  "BASE_URL"
-]
-
-for (const key of required) {
-  if (!CONFIG[key]) {
-    throw new Error(`❌ CONFIG faltando: ${key}`)
-  }
+  FIXED_PAYER_EMAIL:
+    process.env.FIXED_PAYER_EMAIL || "atributosflowlab@gmail.com"
 }
