096e468fa9491947c6a583a5256f747748223d2d
Franke <henriquefranke31@gmail.com>
2026-01-30 21:18:46 -0300

    sg

--- src/config.js
+++ src/config.js
@@ -2,10 +2,13 @@ export const CONFIG = {
   BOT_TOKEN: process.env.BOT_TOKEN,
   GROUP_ID: Number(process.env.GROUP_ID),
   MP_ACCESS_TOKEN: process.env.MP_ACCESS_TOKEN,
+
   VIP_PRICE: Number(process.env.VIP_PRICE || 29.9),
+
   BASE_URL: process.env.BASE_URL,
   PORT: Number(process.env.PORT || 8080)
 }
+
 
 
 // validações obrigatórias
