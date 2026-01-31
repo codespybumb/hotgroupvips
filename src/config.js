c737f60fe123adf97d2b3a37b685519dfecd8f3b
Franke <henriquefranke31@gmail.com>
2026-01-30 22:39:10 -0300

    sg

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 export const CONFIG = {
   BOT_TOKEN: process.env.BOT_TOKEN,
   GROUP_ID: Number(process.env.GROUP_ID),
+    VIP_PRICE: Number(process.env.VIP_PRICE || 29.9),
   MP_ACCESS_TOKEN: process.env.MP_ACCESS_TOKEN,
   FIXED_PAYER_EMAIL: process.env.FIXED_PAYER_EMAIL
-  VIP_PRICE: Number(process.env.VIP_PRICE || 29.9),
 
   BASE_URL: process.env.BASE_URL,
   PORT: Number(process.env.PORT || 8080)
